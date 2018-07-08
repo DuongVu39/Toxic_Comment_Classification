6cad0859db6413611264d0c90807e527739e9234
Tyler Roberts <tcroberts@live.ca>
2018-07-08 14:15:43 -0700

    Fix logger path

--- src/core/DataManager.py
+++ src/core/DataManager.py
@@ -15,7 +15,7 @@ from src import logging_wrapper
 
 class DataManager(object):
 
-    def __init__(self, loc, logging_level=3, logging_path="logs/data_manager{}".format(time.time())):
+    def __init__(self, loc, logging_level=3, logging_path="logs/data_manager{}.txt".format(time.time())):
         self.dat = pd.read_csv(loc)
         self.id_dict = self.dat[['id', 'comment_text']].set_index('id').to_dict()
         self.logger = logging_wrapper(logging_level, logging_path)
