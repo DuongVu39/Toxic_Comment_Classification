4d62f7cd3024c13d5aade40a9dde2cc1555d0cbd
duongvu39 <dqvu@umail.iu.edu>
2018-07-08 22:27:39 -0700

    create a prototype model

--- src/core/models/keras_feed_forward.py
+++ src/core/models/keras_feed_forward.py
@@ -1,1 +1,21 @@
-import keras
+from src.core import DataManager
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import metrics
+
+dm = DataManager("data/train.csv")
+dm.tokenize()
+
+input_shape = dm.dat['tokenized'].as_matrix().shape
+
+
+model = Sequential()
+model.add(Dense(128, activation="relu", input_shape=input_shape))
+model.add(Dense(64, activation="relu"))
+model.add(Dense(16, activation="relu"))
+model.add(Dense(2, activation='softmax'))
+
+
+model.compile(optimizer="adam", loss='categorical_crossentropy',
+              metrics=[metrics.categorical_accuracy, metrics.mae])
+
