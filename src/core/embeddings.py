4a7b43b78d82433ddda590f5e429de1a0e61e2fb
duongvu39 <dqvu@umail.iu.edu>
2018-07-10 23:40:49 -0700

    edit the embedding

--- src/core/embeddings.py
+++ src/core/embeddings.py
@@ -13,15 +13,33 @@ def general_embed(sentence, model):
     new_sentence = [model[i] for i in sentence]
     return new_sentence
 
-
 def create_wtv(corpus, epochs=5):
+    """
+
+    Args:
+        corpus (list): List of all comments
+        epochs (int): Number of epoch
+
+    Returns:
+
+    """
     wtv = Word2Vec(corpus)
     wtv.train(corpus, total_examples=corpus.shape[0], epochs=epochs)
     return wtv
 
 def embed_to_wtv(corpus, epochs=5):
+    """
+
+    Args:
+        corpus (list): List of all comments
+        epochs (int): Number of epoch
+
+    Returns:
+
+    """
     wtv = create_wtv(corpus, epochs=epochs)
-    corpus.apply(general_embed, args=(wtv,))
+    result = corpus.apply(general_embed, args=(wtv,))
+    return result
 
 def embed_to_ft():
     pass
